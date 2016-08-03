0117bff26a746b0c6dfb33bdcdc9520564fce52c
Tobi29 <tobi291019@gmail.com>
2016-08-03 05:16:48 +0200

    Update copyright

--- ScapesCore/src/main/java/org/tobi29/scapes/block/TerrainTextureRegistry.java
+++ ScapesCore/src/main/java/org/tobi29/scapes/block/TerrainTextureRegistry.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2015 Tobi29
+ * Copyright 2012-2016 Tobi29
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -9,10 +9,11 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUTerrainTexture WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.tobi29.scapes.block;
 
 import org.tobi29.scapes.engine.ScapesEngine;
