469c88b9e3842ef63153df594308bcbebd2c6724
Tobi29 <tobi291019@gmail.com>
2017-11-25 15:36:03 +0100

    Use restrictive sandbox for all code

--- ScapesCore/src/main/kotlin/org/tobi29/scapes/plugins/Sandbox.kt
+++ ScapesCore/src/main/kotlin/org/tobi29/scapes/plugins/Sandbox.kt
@@ -16,26 +16,206 @@
 
 package org.tobi29.scapes.plugins
 
-import org.tobi29.scapes.engine.utils.math.threadLocalRandom
+import org.tobi29.scapes.client.ScapesClient
+import org.tobi29.scapes.engine.math.threadLocalRandom
+import org.tobi29.scapes.engine.utils.io.filesystem.FilePath
+import org.tobi29.scapes.engine.utils.logging.KLogging
+import java.io.FilePermission
+import java.lang.reflect.ReflectPermission
+import java.net.SocketPermission
+import java.net.URLClassLoader
 import java.security.*
+import java.util.*
 
-object Sandbox {
+object Sandbox : KLogging() {
     private val whitelist = packageAccess("java.**", "kotlin.**",
             "kotlinx.**", "org.tobi29.scapes.**", "scapes.plugin.**",
             "org.slf4j.**")
     private val permission = RuntimePermission("scapes.restrictedPkg")
     private var sandboxed = false
 
-    fun sandbox() {
+    fun sandbox(accessibleDirectories: Iterable<FilePath>) {
         if (sandboxed) {
             return
         }
         sandboxed = true
         preload()
+        val defaultPermissions = ArrayList<Permission>()
+
+        // Using a proper application sandbox seems better for this
+        logger.debug { "Game permission: File /- read,readlink" }
+        defaultPermissions.add(FilePermission("/-", "read,readlink"))
+        for (path in accessibleDirectories) {
+            val pathStr = path.toAbsolutePath().toString()
+            logger.debug { "Game permission: File $pathStr read,write,delete,readlink" }
+            defaultPermissions.add(
+                    FilePermission(pathStr, "read,write,delete,readlink"))
+            // TODO: Handle - in path
+            val wildcard = "${pathStr.removeSuffix("/")}/-"
+            logger.debug { "Game permission: File $wildcard read,write,delete,readlink" }
+            defaultPermissions.add(
+                    FilePermission(wildcard, "read,write,delete,readlink"))
+        }
+
+        // Allow using all packages
+        defaultPermissions.add(permission)
+
+        // Allow getting metadata from files
+        logger.debug { "Game permission: accessUserInformation" }
+        defaultPermissions.add(RuntimePermission("accessUserInformation"))
+
+        // Allow using plugin jars for assets
+        logger.debug { "Game permission: getClassLoader" }
+        defaultPermissions.add(RuntimePermission("getClassLoader"))
+
+        // Allow using threads
+        logger.debug { "Game permission: modifyThread" }
+        defaultPermissions.add(RuntimePermission("modifyThread"))
+
+        // Log4j2
+        reflectPermission("javax.management.MBeanServerPermission",
+                "createMBeanServer")?.let {
+            logger.debug { "Game permission: MBeanServer createMBeanServer" }
+            defaultPermissions.add(it)
+        }
+        reflectPermission("javax.management.MBeanPermission",
+                "-#-[-]", "queryNames")?.let {
+            logger.debug { "Game permission: MBean -#-[-] queryNames" }
+            defaultPermissions.add(it)
+        }
+        reflectPermission("javax.management.MBeanPermission",
+                "org.apache.logging.log4j.core.jmx.*",
+                "queryNames,unregisterMBean")?.let {
+            logger.debug { "Game permission: MBean org.apache.logging.log4j.core.jmx.* queryNames,unregisterMBean" }
+            defaultPermissions.add(it)
+        }
+
+        // LWJGL
+        logger.debug { "Game permission: accessClassInPackage.sun.misc" }
+        defaultPermissions.add(
+                RuntimePermission("accessClassInPackage.sun.misc"))
+        defaultPermissions.add(
+                RuntimePermission("accessDeclaredMembers"))
+        defaultPermissions.add(
+                ReflectPermission("suppressAccessChecks"))
+
+        // Native libraries
+        val loadLibraries = mutableListOf(
+                "lwjgl",
+                "lwjgl_opengl",
+                "lwjgl_opengles",
+                "lwjgl_stb",
+                "lwjgl_tinyfd")
+        System.getProperty("java.library.path")?.let {
+            // TODO: Verify property
+            loadLibraries.add("$it/libsqlitejdbc.so")
+            logger.debug { "Game permission: File read $it/*" }
+            defaultPermissions.add(FilePermission("$it/*", "read"))
+        }
+        for (loadLibrary in loadLibraries.map { "loadLibrary.$it" }) {
+            logger.debug { "Game permission: $loadLibrary" }
+            defaultPermissions.add(RuntimePermission(loadLibrary))
+        }
+
+        // Temp
+        System.getProperty("java.io.tmpdir")?.let {
+            logger.debug { "Game permission: File read,write,delete,readlink $it/-" }
+            defaultPermissions.add(
+                    FilePermission("$it/-", "read,write,delete,readlink"))
+        }
+
+        // Sandbox
+        logger.debug { "Game permission: getProtectionDomain" }
+        defaultPermissions.add(RuntimePermission("getProtectionDomain"))
+
+        // Scapes
+        logger.debug { "Game permission: scapes.*" }
+        defaultPermissions.add(RuntimePermission("scapes.*"))
+        logger.debug { "Game permission: createClassLoader" }
+        defaultPermissions.add(RuntimePermission("createClassLoader"))
+        logger.debug { "Game permission: closeClassLoader" }
+        defaultPermissions.add(RuntimePermission("closeClassLoader"))
+        logger.debug { "Game permission: Socket * resolve,connect,listen,accept" }
+        defaultPermissions.add(
+                SocketPermission("*", "resolve,connect,listen,accept"))
+        logger.debug { "Game permission: accessClassInPackage.sun.security.pkcs12" }
+        defaultPermissions.add(
+                RuntimePermission("accessClassInPackage.sun.security.pkcs12"))
+        logger.debug { "Game permission: accessClassInPackage.sun.security.jca" }
+        defaultPermissions.add(
+                RuntimePermission("accessClassInPackage.sun.security.jca"))
+        logger.debug { "Game permission: accessClassInPackage.sun.security.util" }
+        defaultPermissions.add(
+                RuntimePermission("accessClassInPackage.sun.security.util"))
+        logger.debug { "Game permission: accessClassInPackage.sun.security.ssl" }
+        defaultPermissions.add(
+                RuntimePermission("accessClassInPackage.sun.security.ssl"))
+        logger.debug { "Game permission: accessClassInPackage.sun.security.rsa" }
+        defaultPermissions.add(
+                RuntimePermission("accessClassInPackage.sun.security.rsa"))
+        logger.debug { "Game permission: accessClassInPackage.sun.security.internal.interfaces" }
+        defaultPermissions.add(
+                RuntimePermission(
+                        "accessClassInPackage.sun.security.internal.interfaces"))
+        logger.debug { "Game permission: accessClassInPackage.sun.security.internal.spec" }
+        defaultPermissions.add(
+                RuntimePermission(
+                        "accessClassInPackage.sun.security.internal.spec"))
+        logger.debug { "Game permission: Security putProviderProperty.SunJCE" }
+        defaultPermissions.add(
+                SecurityPermission("putProviderProperty.SunJCE"))
+        logger.debug { "Game permission: Security putProviderProperty.SunJSSE" }
+        defaultPermissions.add(
+                SecurityPermission("putProviderProperty.SunJSSE"))
+
+        // Allow reading various required properties
+        val readProperties = listOf(
+                "os.arch",
+                "os.name",
+                "os.version",
+                "java.version",
+                "java.vm.name",
+                "java.vm.version",
+                "java.vm.vendor",
+                "java.library.path",
+                "user.name",
+                "java.io.tmpdir",
+                "kotlin.test.is.pre.release",
+                "kotlinx.coroutines.DefaultExecutor.keepAlive",
+                "log4j2.*",
+                "tika.config",
+                "org.apache.tika.*",
+                "org.sqlite.*",
+                "org.lwjgl.*")
+        for (property in readProperties) {
+            logger.debug { "Game permission: Property read $property" }
+            defaultPermissions.add(PropertyPermission(property, "read"))
+        }
+
+        // Allow reading various required environment variables
+        val environmentVariables = listOf(
+                "TURN_OFF_LR_LOOP_ENTRY_BRANCH_OPT",
+                "TIKA_CONFIG")
+        for (variable in environmentVariables) {
+            logger.debug { "Game permission: getenv.$variable" }
+            defaultPermissions.add(RuntimePermission("getenv.$variable"))
+        }
+
+        (ScapesClient::class.java.classLoader as? URLClassLoader)?.let { rootClassLoader ->
+            rootClassLoader.urLs.asSequence().mapNotNull { it.file }.map {
+                logger.debug { "Game permission: File read ($it)" }
+                FilePermission(it, "read")
+            }.forEach { defaultPermissions.add(it) }
+        }
         Policy.setPolicy(object : Policy() {
             override fun getPermissions(codesource: CodeSource?): Permissions {
                 val permissions = Permissions()
-                permissions.add(AllPermission())
+                //permissions.add(AllPermission())
+
+                for (permission in defaultPermissions) {
+                    permissions.add(permission)
+                }
+
                 return permissions
             }
 
@@ -72,6 +252,7 @@ object Sandbox {
     fun preload() {
         // Preload these to avoid permission errors when plugins trip into these
         threadLocalRandom()
+        logger.debug { "Load logging" }
     }
 
     fun packageAccess(vararg str: String): (String) -> Boolean {
@@ -138,4 +319,31 @@ object Sandbox {
         MATCHES,
         ABORT
     }
+}
+
+private inline fun <reified P1> reflectPermission(
+        className: String,
+        parameter1: P1
+): Permission? = reflectPermissionImpl(className,
+        arrayOf(P1::class.java),
+        arrayOf(parameter1))
+
+private inline fun <reified P1, reified P2> reflectPermission(
+        className: String,
+        parameter1: P1,
+        parameter2: P2
+): Permission? = reflectPermissionImpl(className,
+        arrayOf(P1::class.java, P2::class.java),
+        arrayOf(parameter1, parameter2))
+
+private fun reflectPermissionImpl(
+        className: String,
+        types: Array<Class<*>>,
+        parameters: Array<Any?>
+): Permission? = try {
+    val clazz = Class.forName(className)
+    val constructor = clazz.getConstructor(*types)
+    constructor.newInstance(*parameters) as Permission
+} catch (e: Exception) {
+    null
 }
